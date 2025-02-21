330a8ebd979b47510315b109ae26ae75e901daec
klewy <cigankov807@gmail.com>
2025-02-21 07:56:16 +0300

    set fusion

--- main.cpp
+++ main.cpp
@@ -2,10 +2,13 @@
 #include <QApplication>
 #include "dialogs/notecreatedialog.h"
 #include <QDebug>
+#include <QStyleFactory>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    a.setStyle(QStyleFactory::create("Fusion"));
+
     MainWindow w;
 
     NoteCreateDialog dialog;
